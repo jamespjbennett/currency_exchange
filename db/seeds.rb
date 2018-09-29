b018753be89b7a376c83aa26b2965a4239d1ed52
jamespjbennett <jamespjbennett@hotmail.com>
2018-09-29 19:42:37 +0100

    create some seeds for viewing in browser

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Currency.delete_all
+
+
+currency_1 = Currency.create(country_code: 'USD', denomination: '$')
+currency_2 = Currency.create(country_code: 'GBP', denomination: '£')
+
+er1 = ExchangeRate.create(base_currency_id: 1, converted_currency_id: 2, rate: 1.2)
+er2 = ExchangeRate.create(base_currency_id: 2, converted_currency_id: 1, rate: 0.8)
