cc6d3989333c244ccd013d62527db9df1520b80b
jamespjbennett <jamespjbennett@hotmail.com>
2018-09-29 15:46:55 +0100

    currency assosications passing

--- app/models/currency.rb
+++ app/models/currency.rb
@@ -1,2 +1,4 @@
 class Currency < ApplicationRecord
+  has_many :base_currency_rates, :class_name => 'ExchangeRate', :foreign_key => 'base_currency_id'
+  has_many :converted_currency_rates, :class_name => 'ExchangeRate', :foreign_key => 'converted_currency_id'
 end
--- spec/models/currency_spec.rb
+++ spec/models/currency_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Currency, type: :model do
-  it { should have_many(:base_currency_rates) }
-  it { should have_many(:converted_currency_rates) }
+  # before(:each) do
+  #   @currency = create(:currency)
+  # end
+  it { is_expected.to have_many(:base_currency_rates) }
+  it { is_expected.to have_many(:converted_currency_rates) }
+
 end
