2a193a8320ae9a893fa95f05d483baf779fabb0b
jamespjbennett <jamespjbennett@hotmail.com>
2018-09-30 14:55:33 +0100

    failing test for update exchange rate

--- spec/requests/exchange_rate_request_spec.rb
+++ spec/requests/exchange_rate_request_spec.rb
@@ -16,16 +16,30 @@ RSpec.describe "Exchange Rate Requests", :type => :request do
       it "should make the request successfully" do
         expect(response).to be_ok
       end
-      #
+
       it "should incremeent the number of quotations" do
         expect(ExchangeRate.count).to eq(1)
       end
-      #
-      # it "should calculate converted total" do
-      #   expect(JSON.parse(response.body)["converted_total"]).to eq(80)
-      #   expect(JSON.parse(response.body)["formatted_total"]).to eq('£80.00')
-      # end
 
+    end
+
+  end
+
+  describe 'update exchange rates' do
+    before(:each) do
+      base_currency = create(:currency, country_code: 'USD', denomination: '$')
+      converted_currency_id = create(:currency, country_code: 'GBP', denomination: '£')
+      @exchange_rate = create(:exchange_rate, base_currency_id: base_currency.id, converted_currency_id: converted_currency_id.id, rate: 0.8)
+    end
+
+    let(:valid_attributes) { {rate: 0.9 } }
+
+    context 'when the request is valid' do
+      before { patch "/api/v1/exchange_rates/#{@exchange_rate.id}.json", params: valid_attributes }
+
+      it "should make the request successfully" do
+        expect(response).to be_ok
+      end
     end
 
   end
