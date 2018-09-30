832a35dd26fb9e298b3a622ae567cdad24ea21a3
jamespjbennett <jamespjbennett@hotmail.com>
2018-09-30 20:35:27 +0100

    remove duplicate validation

--- spec/models/quotation_spec.rb
+++ spec/models/quotation_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe Quotation, type: :model do
   it "validates" do
     is_expected.to belong_to :exchange_rate
     is_expected.to validate_presence_of(:based_requested_amount)
-    is_expected.to validate_presence_of(:based_requested_amount)
     is_expected.to validate_numericality_of(:based_requested_amount).is_greater_than(0)
   end
 end
