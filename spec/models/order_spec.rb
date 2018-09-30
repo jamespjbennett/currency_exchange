5dfa94e2a7d89066b33f83f6725f924010a7541d
jamespjbennett <jamespjbennett@hotmail.com>
2018-09-30 17:18:04 +0100

    failing assosication and validation tests for order and users

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "validates" do
+    is_expected.to belong_to :exchange_rate
+    is_expected.to belong_to :user
+    is_expected.to validate_presence_of(:user_id)
+    is_expected.to validate_presence_of(:exchange_rate_id)
+    is_expected.to validate_presence_of(:purchase_amount)
+  end
 end
