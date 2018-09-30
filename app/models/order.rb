ba06140b22456bf0a482872ad82351125024708a
jamespjbennett <jamespjbennett@hotmail.com>
2018-09-30 17:41:06 +0100

    order value successfully transferred

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,12 @@ class Order < ApplicationRecord
   include ActiveSupport::NumberHelper
   belongs_to :exchange_rate
   belongs_to :user
-  validates :user_id,:exchange_rate_id, presence: true
+  belongs_to :quotation
+  validates :user_id,:exchange_rate_id, :quotation_id, presence: true
+  after_create :transfer_purchase_amount
+
+  def transfer_purchase_amount
+    update(purchase_amount: quotation.converted_total)
+  end
 
 end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -6,5 +6,6 @@ RSpec.describe Order, type: :model do
     is_expected.to belong_to :user
     is_expected.to validate_presence_of(:user_id)
     is_expected.to validate_presence_of(:exchange_rate_id)
+    is_expected.to validate_presence_of(:quotation_id)
   end
 end
