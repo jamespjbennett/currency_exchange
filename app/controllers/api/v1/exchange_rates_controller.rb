d97bb7ab026bd59febe3ded2f0677c8ce010b7de
jamespjbennett <jamespjbennett@hotmail.com>
2018-09-30 14:57:44 +0100

    passing test for update exchange rate

--- app/controllers/api/v1/exchange_rates_controller.rb
+++ app/controllers/api/v1/exchange_rates_controller.rb
@@ -1,14 +1,26 @@
 module Api::V1
   class ExchangeRatesController < ApplicationController
+    before_action :set_exchange_rate, only: [:update]
+
     def create
       @exchange_rate = ExchangeRate.create(exchange_rate_params)
-      render json: @exchange_rate.to_json
+      render json: @exchange_rate
+    end
+
+    def update
+      @exchange_rate.update(exchange_rate_params)
+      render json: @exchange_rate
     end
 
     private
+
     def exchange_rate_params
       # whitelist params
       params.permit(:base_currency_id, :converted_currency_id, :rate)
+    end
+
+    def set_exchange_rate
+      @exchange_rate = ExchangeRate.find(params[:id])
     end
 
   end
