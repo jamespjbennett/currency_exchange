c95f29d4d1cd249f5791e2f01dce2c6dbbb3694e
jamespjbennett <jamespjbennett@hotmail.com>
2018-09-30 14:50:02 +0100

    passing test for exchange rate creation

--- app/controllers/api/v1/exchange_rates_controller.rb
+++ app/controllers/api/v1/exchange_rates_controller.rb
@@ -1,7 +1,15 @@
 module Api::V1
   class ExchangeRatesController < ApplicationController
     def create
-      render json: {success: true}
+      @exchange_rate = ExchangeRate.create(exchange_rate_params)
+      render json: @exchange_rate.to_json
     end
+
+    private
+    def exchange_rate_params
+      # whitelist params
+      params.permit(:base_currency_id, :converted_currency_id, :rate)
+    end
+
   end
 end
