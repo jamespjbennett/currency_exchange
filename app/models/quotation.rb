cf2eab06b3d276825d5fd1d83b846beecdd2022a
jamespjbennett <jamespjbennett@hotmail.com>
2018-10-01 20:35:13 +0100

    passing test for transferring currennt exchange rate

--- app/models/quotation.rb
+++ app/models/quotation.rb
@@ -3,11 +3,16 @@ class Quotation < ApplicationRecord
   belongs_to :exchange_rate
   validates :based_requested_amount, presence: true
   validates :based_requested_amount, numericality: { greater_than: 0 }
-  after_create :calculate_converted_total
+  after_create :calculate_converted_total, :store_current_exhchange_rate
 
   # CALCULATED THE QUOTATION AMOUNT BASED ON THE BASED AMOUNT AND EXCHANGE RATE
   def calculate_converted_total
     update(converted_total: based_requested_amount * exchange_rate.rate)
+  end
+
+  # STORE CURRENT EXCHANGE RATE TO COMPARE WHEN CONVERTED TO ORDER
+  def store_current_exhchange_rate
+    update(current_exchange_rate: exchange_rate.rate)
   end
 
   # GET THE COUNTRY CODE OF THE CONVERTED CURRENCY
@@ -36,6 +41,7 @@ class Quotation < ApplicationRecord
       requested_base_currency_amount: based_requested_amount,
       converted_total: converted_total,
       formatted_total: formatted_total,
+      current_exchange_rate: current_exchange_rate,
     })
   end
 
