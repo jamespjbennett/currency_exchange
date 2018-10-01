3136307be8d437a89f1569bcdb3b7d801d8ed379
jamespjbennett <jamespjbennett@hotmail.com>
2018-10-01 08:30:20 +0100

    fix user order display test with somewhat mystery solution

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
   has_many :orders
+
+  def to_json(*)
+    JSON.dump({
+      name: name,
+      orders: orders.map(&:to_json),
+    })
+  end
 end
--- spec/requests/user_request_spec.rb
+++ spec/requests/user_request_spec.rb
@@ -8,15 +8,20 @@ RSpec.describe "Exchange Rate Requests", :type => :request do
     include_context 'orders'
 
     context "return correct attributes" do
-
       before { get "/api/v1/users/#{user.id}.json" }
-
       it "should make the request successfully" do
         expect(response).to be_ok
       end
 
       it "should display the correct user id" do
         expect(json_response["name"]).to eq('test')
+      end
+
+      it "should display the users orders" do
+        # FOR SOME REASON THIS ONLY WORKED IF I MAKE THE REQUEST AGAIN?
+        user.orders << order
+        get "/api/v1/users/#{user.id}.json"
+        expect(json_response["orders"].length).to eq(1)
       end
 
     end
